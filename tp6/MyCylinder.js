1e1e9b30e8c492d4b702a9b2f3397060e77efd81
Ricardo Ferreira <ricardoferreira@skin.pt>
2018-05-04 08:10:35 +0100

    Add new MyCylinder

--- tp6/MyCylinder.js
+++ tp6/MyCylinder.js
@@ -1,86 +1,54 @@
-/**
- * MyObject
- * @param gl {WebGLRenderingContext}
- * @constructor
- */
-
 class MyCylinder extends CGFobject
 {
-	constructor(scene, slices, stacks)
-	{
-		super(scene);
-		this.slices = slices; this.stacks = stacks;
-		this.initBuffers();
-};
+  constructor(scene, slices, stacks)
+  {
+    super(scene);
+    this.slices = slices;
+    this.stacks = stacks;
+    this.initBuffers();
+  };
+
+  placeVertexAndNormals(z){
+    let ang = 0;
+
+    this.normals.push(Math.cos(0),Math.sin(0),0);
+    this.vertices.push(Math.cos(ang*Math.PI/180),Math.sin(ang*Math.PI/180),z);
+    this.texCoords.push(0,z);
+    for(let i=1;i<this.slices;i++){
+      ang+=360/this.slices;
+      this.vertices.push(Math.cos(ang*Math.PI/180),Math.sin(ang*Math.PI/180),z);
+      this.normals.push(Math.cos(ang*Math.PI/180),Math.sin(ang*Math.PI/180),0);
+      this.texCoords.push(i*(1/this.slices),z);
+
+    }
+    this.vertices.push(Math.cos(0),Math.sin(0),z);
+    this.normals.push(Math.cos(0),Math.sin(0),0);
+    this.texCoords.push(1,z);
+  };
+
 	initBuffers()
 	{
-		let n, i, k;
-		this.indices = [];
 		this.vertices = [];
-		this.normals = [];
-		this.texCoords = [];
-		for(k=0;k<this.stacks;k++){
-			let vertex = [];
-			let norm = [];
-			for(n=0;n<=1;n++){
-				for(i = 0; i < this.slices*3; i+=3){
-					let x = Math.cos((2*Math.PI*(i/3))/this.slices);
-					let y = Math.sin((2*Math.PI*(i/3))/this.slices);
-					vertex.push(x ,y ,n*(1/this.stacks)+k*(1/this.stacks));
-					norm.push(x ,y ,0);
-				}
-			}
-			let dupV = vertex.slice(0);
-			let temp = vertex.concat(dupV);
-			this.vertices = this.vertices.concat(temp);
-			let dupN = norm.slice(0);
-			let tempN = norm.concat(dupN);
-			this.normals = this.normals.concat(tempN);
-		}
+    this.indices = [];
+    this.normals = [];
+    this.texCoords = [];
 
-		for(n=0;n<this.stacks;n++){
-			let k=n*this.slices*4;
-			let second = true;
-			for(i = 0 ; i < this.slices*6; i+=6){
-				if(k+1==(this.slices*3+n*4*this.slices)){
-					this.indices.push(k, k+1-this.slices, k+1);
-					this.indices.push(k+1, k+this.slices, k);
-				}
-				else if(k+1==this.slices+n*this.slices*4 && (this.slices % 2) != 0){
-					this.indices.push(k, k+1+this.slices, k+1+2*this.slices);
-					this.indices.push(k+1+2*this.slices, k+this.slices, k);
-				}
-				else{
-					this.indices.push(k, k+1, k+1+this.slices);
-					this.indices.push(k+1+this.slices, k+this.slices, k);
-					k++;
-					if(second){
-						k+=this.slices*2;
-						second = false;
-					}
-					else {
-						k-=this.slices*2;
-						second = true;
-					}
-				}
-			}
-		}
-		for(k = 0; k<this.stacks;k++){
-			for(i = 0; i<this.slices;i++){
-				this.texCoords.push(i/this.slices,0+k*1/this.stacks);
-			}
-			for(i = 0; i<this.slices;i++){
-				this.texCoords.push(i/this.slices,1/this.stacks+k*1/this.stacks);
-			}
-			this.texCoords.push(1,0+k*1/this.stacks);
-			for(i = 1; i<this.slices;i++){
-				this.texCoords.push(i/this.slices,0+k*1/this.stacks);
-			}
-			this.texCoords.push(1,1/this.stacks+k*1/this.stacks);
-			for(i = 1; i<this.slices;i++){
-				this.texCoords.push(i/this.slices,1/this.stacks+k*1/this.stacks);
-			}
-		}
+    this.placeVertexAndNormals(1);
+
+    let index = 0;
+    for(let j=0; j<this.stacks; j++){
+      let z = 1-(j+1)/this.stacks;
+      this.placeVertexAndNormals(z);
+      for(let i=0; i<this.slices ; i++){
+        this.indices.push(index,index+this.slices+1,index+1)
+        this.indices.push(index+this.slices+1,index+this.slices+2,index+1)
+        index++;
+      }
+      index++;
+    }
+
+    console.log(this.vertices);
+
 		this.primitiveType=this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
 	};
