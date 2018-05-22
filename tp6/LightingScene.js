890e6262020fc6227b6710d23bb5453eefb12b8b
Filipe Lemos <32512377+FilipeFLemos@users.noreply.github.com>
2018-05-22 11:41:44 +0100

    texturas do novo carro correspondem às opcoes atuais

--- tp6/LightingScene.js
+++ tp6/LightingScene.js
@@ -1,18 +1,10 @@
-var degToRad = Math.PI / 180.0;
-
-var BOARD_WIDTH = 6.0;
-var BOARD_HEIGHT = 4.0;
-
-var BOARD_A_DIVISIONS = 30;
-var BOARD_B_DIVISIONS = 100;
-
-var FPS = 10;
 
 class LightingScene extends CGFscene
 {
-	constructor()
+	constructor(myInterface)
 	{
 		super();
+		this.myInterface = myInterface;
 	};
 
 	init(application)
@@ -31,28 +23,59 @@ class LightingScene extends CGFscene
 		this.gl.enable(this.gl.CULL_FACE);
 		this.gl.depthFunc(this.gl.LEQUAL);
 
+		//Axis
 		this.axis = new CGFaxis(this);
-
-		// Scene elements
-		this.terrain = new MyTerrain(this);
-
-		//Wheel
-		this.chassis = new MyChassis(this,16,20);
 
 		// Materials
 		this.materialDefault = new CGFappearance(this);
 
-		this.car = new MyVehicle(this);
+		//External Solids
+		this.external = new SolidosExternos(this);
 
-		this.clocktexture = new CGFappearance(this);
-    this.clocktexture.setAmbient(0.8,0.8,0.8,1);
-		this.clocktexture.setDiffuse(0.8,0.8,0.8,1);
-		this.clocktexture.setSpecular(0.1,0.1,0.1,1);
-		this.clocktexture.setShininess(120);
-    this.clocktexture.loadTexture('../resources/images/lamp.jpg');
+		//altimetry
+						//	   0	   1	 2    3    4    5    6    7     8
+        this.altimetry= [ 	[ 12.0 , 12.0 , 0.0, 0.0, 8.0, 8.0, 0.0, 0.0, 32.0 ],	//0
+            				[ 18.0 , 18.0 , 0.0, 0.0, 8.0, 8.0, 0.0, 0.0, 32.0 ],	//1
+            				[ 32.0 , 32.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 32.0 ],	//2
+            				[ 32.0 , 32.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 32.0 ],	//3
+            				[ 24.0 , 24.0 , 0.0, 0.0, 12.0, 12.0, 0.0, 0.0, 18.0 ],	//4
+            				[ 18.0 , 18.0 , 0.0, 0.0, 24.0, 24.0, 0.0, 0.0, 18.0 ],	//5
+            				[ 12.0 , 12.0 , 0.0, 0.0, 18.0, 18.0, 0.0, 0.0, 12.0 ],	//6
+            				[ 18.0 , 18.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 32.0 ],	//7
+            				[ 12.0 , 12.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 32.0 ] 	//8
+        				];
 
 
-		this.setUpdatePeriod(1000/FPS);
+		// Terrain elements
+        let altSize = this.altimetry.length;
+		this.terrain = new MyTerrain(this, altSize-1, this.altimetry);
+
+		//Vehicle
+		this.vehicleXPos = -5;
+		this.vehicleYpos = 1.40;
+		this.vehicleZPos = -10;
+        //vehicles Array
+        this.activeVehicle = 0; //indice
+        this.vehicles = [];
+        this.vehicle = new MyVehicle(this);
+        this.vehicles.push(this.vehicle);
+
+		//Crane
+		//Please specify here the crane position, don't change anywhere else
+		    this.craneXpos = 5;
+		    this.craneYPos = 1.3;
+		    this.craneZPos = -20;
+		    this.craneRotation = 90*degToRad;
+		this.crane = new MyCrane(this,this.craneXpos,this.craneYPos,-this.craneZPos, this.craneRotation);
+		this.craneDisplaysCar = false;
+
+
+		this.setTextureDropDown();
+
+		//FPS
+		this.FPS = 100;
+		this.oldFps = 100;
+		this.setUpdatePeriod(1000/this.FPS);
 
 		//Luzes
 		this.luz1 = true;
@@ -64,23 +87,213 @@ class LightingScene extends CGFscene
 		this.luzes = false;
 
 		//options
-		this.showAxis=false;
+		this.showAxis=true;
 		this.option1=true;
 		this.option2=false;
-		this.speed=3;
 
+		this.keyAPressed = false;
+		this.keyDPressed = false;
+		this.keyWPressed = false;
+		this.keySPressed = false;
+
+		this.rotation=0;
+		this.steering = 0;
+		this.speed=0.1;
+		this.compensateDirection = false;
 	};
 
-	doSomething(){
-		console.log("Doing something...");
+	setTextureDropDown(){
+		this.textureMap = new Map();
+		this.textureMap.set(0,"Body");
+		this.textureMap.set(1,"Windows");
+		this.textureMap.set(2,"Wheels");
+		this.textureMap.set(3,"Headlights");
+		this.textures = [];
+
+		this.Body = "redbull";
+		this.Windows = "glass";
+		this.Wheels = "wheelTexture";
+		this.Headlights = "lamp";
+
+		this.currentVehicleAppearance = [];
+		this.vehicleAppearances = [];
+		this.vehicleAppearecesList = [];
+
+		this.fillAppearancesObjects(0,"redBull.jpg","dakar.jpg","redbull","dakar");
+		this.fillAppearancesObjects(1,"glass.png","magnet.jpg","glass","magnet");
+		this.fillAppearancesObjects(2,"wheelTexture.jpg","pointer.png","wheelTexture","pointer");
+		this.fillAppearancesObjects(3,"lamp.jpg","donald.png","lamp","donald");
 	}
 
+	fillAppearancesObjects(index, textureFileName1, textureFileName2, texture1, texture2){
+		let text1 = new CGFappearance(this);
+  	text1.setAmbient(0.8,0.8,0.8,1);
+		text1.setDiffuse(0.8,0.8,0.8,1);
+		text1.setSpecular(0.1,0.1,0.1,1);
+		text1.setShininess(120);
+  	text1.loadTexture('../resources/images/'+textureFileName1);
+
+		let text2 = new CGFappearance(this);
+    text2.setAmbient(0.8,0.8,0.8,1);
+		text2.setDiffuse(0.8,0.8,0.8,1);
+		text2.setSpecular(0.1,0.1,0.1,1);
+		text2.setShininess(120);
+		text2.loadTexture('../resources/images/'+textureFileName2);
+
+		this.vehicleAppearances[index] = [text1, text2];
+		this.vehicleAppearecesList[index] = new Map();
+		this.vehicleAppearecesList[index].set(texture1, 0);
+		this.vehicleAppearecesList[index].set(texture2, 1);
+
+		this.currentVehicleAppearance[index] = this.vehicleAppearecesList[index].get(texture1);
+	}
+
+	checkKeys() {
+		if (this.gui.isKeyPressed("KeyW") && !this.crane.shouldDisplayCar())
+		{
+				if(this.speed<1)
+						this.speed+=0.01;
+		}
+		if (this.gui.isKeyPressed("KeyS") && !this.crane.shouldDisplayCar())
+		{
+				if(this.speed>-1)
+					this.speed-=0.01;
+		}
+		if (this.gui.isKeyPressed("KeyA"))
+		{
+			this.compensateDirection=false;
+			this.keyAPressed=true;
+			if(this.rotation<34){
+					this.rotation+=2;
+			}
+			this.steering+=10*this.speed;
+		}
+
+	if(this.keyAPressed && !this.gui.isKeyPressed("KeyA")){
+		this.keyAPressed = false;
+		this.compensateDirection=true;
+	}
+
+	if (this.gui.isKeyPressed("KeyD"))
+		{
+			this.compensateDirection=false;
+			this.keyDPressed=true;
+			if(this.rotation>-34){
+					this.rotation-=2;
+			}
+			this.steering-=10*this.speed;
+		}
+
+		if(this.keyDPressed && !this.gui.isKeyPressed("KeyD")){
+			this.keyDPressed = false;
+			this.compensateDirection=true;
+		}
+
+		if (this.gui.isKeyPressed("KeyZ")){
+			this.speed=0;
+		}
+
+}
+
 	update(currTime){
+
 		this.lastTime = this.lastTime || 0;
 
 		this.deltaTime = currTime - this.lastTime;
 		this.lastTime = currTime;
 
+		//Update FPS
+		if(this.FPS!=this.oldFps){
+			this.setUpdatePeriod(1000/this.FPS);
+			this.oldFps = this.Fps;
+		}
+
+		//Update texture car
+		this.textures[0] = this.Body;
+		this.textures[1] = this.Windows;
+		this.textures[2] = this.Wheels;
+		this.textures[3] = this.Headlights;
+
+		for(let i=0; i<this.textures.length; i++){
+			let index = this.vehicleAppearecesList[i].get(this.textures[i]);
+			if(index!=this.currentVehicleAppearance[i]){
+				this.currentVehicleAppearance[i] = index;
+				this.vehicles[this.activeVehicle].updateCarTexture(this.textureMap.get(i),this.vehicleAppearances[i][index]);
+			}
+		}
+
+		//Update crane angle
+		let xDif =  Math.abs(this.vehicles[this.activeVehicle].getXpos() - this.crane.getRXPosition());
+		xDif += this.vehicleXPos;
+		//console.log("xDif" + xDif);
+		let zDif =  Math.abs(this.vehicles[this.activeVehicle].getZpos() - this.crane.getRZPosition());
+		zDif += this.vehicleZPos;
+		//console.log("RXPos: " + this.crane.getRXPosition());
+        //console.log("RZPos: " + this.crane.getRZPosition());
+
+		//console.log("zDif" + zDif);
+		//console.log("Value -> " + (xDif<2 && zDif<2 && this.speed==0));
+		if(xDif<2 && zDif<2 && this.speed==0){
+			this.crane.update(this.deltaTime);
+			if(this.crane.getCurrentState()==6){
+                this.crane.releaseVehicle(this.vehicles[this.activeVehicle], this.steering);
+				this.crane.setState(0);
+			}
+		}
+
+		//Update car
+		this.checkKeys();
+
+		if(this.rotation == 0){
+			this.compensateDirection=false;
+		}
+
+		if(this.compensateDirection){
+			if(this.rotation>0){
+				this.rotation-=2;
+				this.steering+=10*this.speed;
+			} else {
+				this.rotation+=2;
+				this.steering-=10*this.speed;
+			}
+		}
+
+		//prevent speed when crane has the car
+		if(!this.crane.shouldDisplayCar()){
+			this.craneDisplaysCar = false;
+            this.myInterface.speedSlider.domElement.style.pointerEvents = "click";
+            this.myInterface.speedSlider.domElement.style.opacity = 1;
+		} else {
+            this.myInterface.speedSlider.domElement.style.pointerEvents = "none";
+            this.myInterface.speedSlider.domElement.style.opacity = .5;
+            this.craneDisplaysCar = true;
+		}
+
+		this.vehicles[this.activeVehicle].update(this.speed, this.steering, this.rotation);
+
+        if(this.vehicles[this.activeVehicle].carDead){
+            this.vehicleXPos = -5;
+            this.vehicleYpos = 1.40;
+            this.vehicleZPos = -10;
+            this.steering=0;
+            let v = new MyVehicle(this);
+            this.vehicles.push(v);
+            this.activeVehicle+=1;
+						this.resetTextures();
+        }
+	}
+
+	resetTextures(){
+		this.textures[0] = this.Body;
+		this.textures[1] = this.Windows;
+		this.textures[2] = this.Wheels;
+		this.textures[3] = this.Headlights;
+
+		for(let i=0; i<this.textures.length; i++){
+			let index = this.vehicleAppearecesList[i].get(this.textures[i]);
+			this.currentVehicleAppearance[i] = index;
+			this.vehicles[this.activeVehicle].updateCarTexture(this.textureMap.get(i),this.vehicleAppearances[i][index]);
+		}
 	}
 
 	initCameras()
@@ -93,7 +306,7 @@ class LightingScene extends CGFscene
 		this.setGlobalAmbientLight(1,1,1, 1.0);
 
 		// Positions for four lights
-		this.lights[0].setPosition(4.0, 6.0, 1.0, 1.0);
+		this.lights[0].setPosition(-12.0, 5.0, 12.0, 1.0);
 		//this.lights[0].setVisible(true);
 		this.lights[0].setSpecular(1.0,1.0,1.0,1.0);
 		this.lights[0].setAmbient(0, 0, 0, 1.0);
@@ -102,7 +315,8 @@ class LightingScene extends CGFscene
 		this.lights[0].setLinearAttenuation(0);
 		this.lights[0].setQuadraticAttenuation(0);
 
-		this.lights[1].setPosition(10.5, 6.0, 1.0, 1.0);
+		this.lights[1].setPosition(-12, 5.0, -12.0, 1.0);
+		this.lights[1].setSpecular(1.0, 1.0, 1.0, 1.0);
 		//this.lights[1].setVisible(true);
 		this.lights[1].setAmbient(0, 0, 0, 1.0);
 		this.lights[1].setDiffuse(1.0, 1.0, 1.0, 1.0);
@@ -110,22 +324,23 @@ class LightingScene extends CGFscene
 		this.lights[1].setLinearAttenuation(0);
 		this.lights[1].setQuadraticAttenuation(0);
 
-		this.lights[2].setPosition(10.5, 6.0, 5.0, 1.0);
+		this.lights[2].setPosition(12.0, 5.0, 12.0, 1.0);
 		this.lights[2].setSpecular(1.0, 1.0, 1.0, 1.0);
 		//this.lights[2].setVisible(true);
 		this.lights[2].setAmbient(0, 0, 0, 1.0);
 		this.lights[2].setDiffuse(1.0, 1.0, 1.0, 1.0);
-		this.lights[2].setConstantAttenuation(0);
-		this.lights[2].setLinearAttenuation(1.0);
+		this.lights[2].setConstantAttenuation(1.0);
+		this.lights[2].setLinearAttenuation(0);
 		this.lights[2].setQuadraticAttenuation(0);
 
-		this.lights[3].setPosition(4.0, 6.0, 5.0, 1.0);
+		this.lights[3].setPosition(12.0, 5.0, -12.0, 1.0);
+		this.lights[3].setSpecular(1.0, 1.0, 1.0, 1.0);
 		//this.lights[3].setVisible(true);
 		this.lights[3].setAmbient(0, 0, 0, 1.0);
 		this.lights[3].setDiffuse(1.0, 1.0, 1.0, 1.0);
-		this.lights[3].setConstantAttenuation(0);
+		this.lights[3].setConstantAttenuation(1.0);
 		this.lights[3].setLinearAttenuation(0);
-		this.lights[3].setQuadraticAttenuation(0.1);
+		this.lights[3].setQuadraticAttenuation(0);
 
 	};
 
@@ -191,16 +406,53 @@ class LightingScene extends CGFscene
 
 		// ---- BEGIN Scene drawing section
 
+		//this.pos-=0.1;
 		this.pushMatrix();
 			this.terrain.display();
 		this.popMatrix();
 
-		this.pushMatrix();
-			this.translate(0,1.3,0);
-			this.car.display();
-		this.popMatrix();
-		this.car.controlLights(this.luzes);
+		// this.pushMatrix();
+         // 	this.translate(15,1.35,15);
+         // 	this.rotate(90*degToRad,0,1,0);
+		// 	this.vehicle.display();
+		// this.popMatrix();
+		// this.vehicle.controlLights(this.luzes);
 
+
+		//Display active vehicles
+        if(!this.craneDisplaysCar){
+			this.pushMatrix();
+				this.translate(this.vehicleXPos,this.vehicleYpos,this.vehicleZPos);
+				//this.translate(0,1.35,0);
+				//this.rotate(90*degToRad,0,1,0);
+				this.vehicles[this.activeVehicle].display();
+			this.popMatrix();
+			this.vehicles[this.activeVehicle].controlLights(this.luzes);
+        }
+
+        this.pushMatrix();
+			this.materialDefault.apply();
+			this.translate(this.craneXpos,this.craneYPos,this.craneZPos);
+        	//this.translate(0,1.3,0);
+        	this.rotate(-this.craneRotation,0,1,0);
+			this.crane.display(this.vehicles[this.activeVehicle]);
+        this.popMatrix();
+
+        //Display dead vehicles
+       for(let i=0; i<this.activeVehicle;i++){
+            this.pushMatrix();
+            this.translate(this.vehicleXPos,this.vehicleYpos,this.vehicleZPos);
+            //this.translate(0,1.35,0);
+            //this.rotate(90*degToRad,0,1,0);
+            this.vehicles[i].display();
+            this.popMatrix();
+            //this.vehicles[i].controlLights(this.luzes);
+        }
+
+				this.pushMatrix();
+				this.translate(0,0,30);
+				this.external.display();
+				this.popMatrix();
 		// ---- END Scene drawing section
 	};
 };
